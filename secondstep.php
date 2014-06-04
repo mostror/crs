bb45df957b9b59629f5aebf604b842a15726a992
Julian Sanchez <frankieesanchez@gmail.com>
2014-06-04 16:14:54 -0300

    bug fixed

--- secondstep.php
+++ secondstep.php
@@ -2,37 +2,43 @@
 	session_start();
 	if(!isset($_SESSION['id']))
 		header("Location:index.php");
-	
+
 	if($_SESSION['second'] == 1)
 		header("Location:home.php");
-	
+
 	include "funciones.php";
 	conectar();
-	
+
 	$query=mysql_query($string = "SELECT `password`, `hash`, `validhashtime` FROM users WHERE username='".$_SESSION['username']."'") or die ($string);
-	
+
 	$query=mysql_fetch_assoc($query);
-	
+
 	$hash=$query['hash'];
 	$hashvt=$query['validhashtime'];
-	
-	
+
+
 	if (isset($_POST['hash'])){
 		mysql_query("UPDATE users SET hash = null, validhashtime = null WHERE id=".$_SESSION['id']);
-		if ($hash == $_POST['hash']){
+		if ($hash == $_POST['hash'] && $hashvt == (strtotime(date("Y-m-d H:i:s")) - time()) < 0){
 			$_SESSION['second']=1;
 			header("Location:home.php");
 		}
 		else
-		{
-			session_destroy();
-			echo "Invalid hash. please click <a href=\"index.php\">here</a> to log in again.";
-			die("");
-		}
+			if ($hash != $_POST['hash']) {
+				session_destroy();
+				echo "Invalid hash. please click <a href=\"index.php\">here</a> to log in again.";
+				die("");
+			}
+			else
+			{
+				session_destroy();
+				echo "Hash timed out. please click <a href=\"index.php\">here</a> to log in again.";
+				die("");
+			}
 	}
-	
-	
-	
+
+
+
 ?>
 <form name="formLogin" method="post" action="secondstep.php">
 
@@ -42,24 +48,22 @@
 	<input type="submit" value="Login" id="buttonLogin">
 </form>
 <?php
-	
+
 			//echo "diferencia: ".(strtotime($hashvt) - time());
-	
+
 	if (($hash == null OR ((strtotime($hashvt) - time()) <= 0)) AND (!isset($_POST['hash']))){
-		
+
 		if ((strtotime($hashvt) - time()) > 0)
 		{
 			echo "The last hash has expired, you will receive a new one.<br />"; 
 		}
-				
+
 		$newhash=md5($_SESSION['email'].$query['password'].$_SESSION['username'].date("Y-m-d H:i:s").rand(0,100000));
 		$newvalidhashdate = date("Y-m-d H:i:s", strtotime("+2 minutes"));
 		mysql_query("UPDATE users SET hash = '$newhash', validhashtime = '$newvalidhashdate' WHERE id=".$_SESSION['id']);
-		
+
 		sendhash($newhash, $_SESSION['email']);
-				
+
 	}
-	
-	//mail: 2060.js@gmail.com / pass: navemostro
-	
+
 ?>
